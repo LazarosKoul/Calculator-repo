65efcb95c49e9033c52541e77e5b1c0c05213a27
LazarosKoul <lazoskoul@gmail.com>
2022-10-11 07:24:58 +0300

    Initial Commit

---
+++ Calculator/src/AdderTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AdderTest {
+	
+	Calculator mycal = new Calculator();
+	
+	@Test
+	void testAdd() {
+		int res = mycal.add(3, 4);
+		assertEquals( res, 7);
+	}
+	
+	@Test
+	void testMul() {
+		int res = mycal.multiply(3, 4);
+		assertEquals(res, 12);
+	}
+}
---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,11 @@
+
+public class Calculator {
+
+	public int add(int one, int two) {
+		return one + two;
+	}
+	
+	public int multiply(int one, int two) {
+		return one * two;
+	}
+}
